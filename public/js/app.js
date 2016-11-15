b1d5929852c7aaec5c5eac6fb02a92f7dba94713
Farooq Toppa <farooqtoppa@gmail.com>
2016-11-14 19:50:15 -0500

    full crud baby

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,36 @@
 (function(){
-  angular.module('BookCollection', ['ui.router'])
+  angular.module('bookCollection', ['ui.router'])
   .config(MainRouter);
 
-  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider){
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+    .state('newbook', {
+      url: '/newbook',
+      templateUrl: 'partials/newbook.html'
+    })
+    .state('allbooks', {
+      url: '/allbooks',
+      templateUrl: 'partials/allbooks.html'
+    })
+    .state('editbooks',{
+      url: '/editbooks',
+      templateUrl: 'partials/editbooks.html'
+    })
+    .state('deletebooks',{
+      url: '/deletebooks',
+      templateUrl: 'partials/deletebooks.html'
+    });
 
+     $urlRouterProvider.otherwise('/');
+
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+      });
   }
+
 
 })()
 console.log('app.js');
--- public/js/bookController.js
+++ public/js/bookController.js
@@ -1,33 +1,96 @@
-(function(){
 
-  console.log("===== API talking port 3000 =====");
-  angular.module('BookCollection')
-    .controller('BookController', BookController);
+(function() {
+  angular.module('bookCollection')
+ .controller('BookController',BookController);
 
-    BookController.$inject = ['$http','$state'];
+ function BookController($http, $state) {
+  var self = this;
+  var rootUrl = "http://localhost:3000";
 
-    function BookController($http, $state){
+  self.getAllBooks = getAllBooks;
+  self.createTask = createTask;
+  self.deleteOneTask = deleteOneTask;
+  self.update = update;
+  //self.updateTask = updateTask;
 
-    var self = this;
-    var rootUrl = "http://localhost:3000";
+  // invoke funciton to get books here
+  getAllBooks()
 
-    function getAllBooks(){
+    // ALL BOOKS
+    function getAllBooks() {
       $http({
         method: 'GET',
         url: `${rootUrl}/api/books`,
-        data: books,
+        data: {},
         responseType: 'json'
       })
-      .then(function(response){
-        self.books = response.data;
-        console.log('Books: ', response.data);
+      .then(function(response) {
+        self.bookList = response.data.books;
+        console.log('Books', response.data);
       })
       .catch((err) => { console.log(err) });
+    } // ends getbooks()
+
+    // CREATE BOOKS
+    function createTask(detail) {
+      console.log("detail ", detail);
+      $http({
+        method: 'POST',
+        url: `${rootUrl}/api/books`,
+        data: {
+          title: detail.title,
+          author: detail.author,
+          publisher: detail.publisher
+        }
+      })
+      .then(function(response) {
+        console.log('Response ', response.data);
+        self.bookList.push(response.data.book);
+        self.detail ='';
+        $state.go('allbooks');
+      })
+      .catch((err) => { console.log(err) });
+    } // ends createbook()
+
+    // DELETE BOOKS
+    function deleteOneTask(i) {
+      console.log(`deleteOneTask function call line ${i}`);
+      $http({
+        method: 'DELETE',
+        url: `${rootUrl}/api/books/${self.bookList[i].id}`,
+        data: {},
+        responseType: 'json'
+      })
+      .then(function(response) {
+        console.log("RESPONSE-deleteOneTask", response.data);         // test - 2B deleted
+        self.bookList.splice(i,1);
+
+        $state.go('editbooks');
+      })
+      .catch((err) => { console.log(err) });
+
+    } // ends delete function()
+
+    function update (book){
+        $http({
+        url: `${rootUrl}/api/books/${book.id}`,
+        method: 'PUT',
+        data: book
+      })
+      .then(function(response){
+        book.title = response.data.book.title
+        book.author = response.data.book.author
+        book.publisher = response.data.book.publisher
+        console.log(book);
+
+        $state.go('allbooks');
+
+      })
+      .catch(function(error){
+        console.log(error);
+      })
     }
 
+  } // ends Book Controller ()
 
-
-  }
-
-
-}) ()
+})() // ends IIFFEE
